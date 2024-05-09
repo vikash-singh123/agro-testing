f9a5226d5d4f91fd76b74378869bc5a03605c636
Vikash Kumar Singh <92633417+vikash-singh123@users.noreply.github.com>
2024-05-09 17:45:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "self-host-eks-dev"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  hash_key       = "LockID"
-  read_capacity  = 5
-  write_capacity = 5
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
 module "eks_cluster" {
   source = "./EKS"
